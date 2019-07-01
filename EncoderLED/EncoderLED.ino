711a42173dfd270658da21659cbddccba0c80532
tigoe <tom.igoe@gmail.com>
2019-07-01 16:04:37 +0800

    Create EncoderLED.ino

---
+++ EncoderLED/EncoderLED.ino
@@ -0,0 +1,27 @@
+/* Encoder LED dimmer
+    based on Paul Stoffregen's Encoder Basic example
+*/
+
+#include <Encoder.h>
+Encoder myEncoder(0, 1);
+int lastPosition  = -1;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the encoder:
+  long encoderPos = myEncoder.read();
+  // check to see if it has changed:
+  if (encoderPos != lastPosition) {
+    // constrain the encoder to a 0-255 range:
+    newPosition = constrain(encoderPos, 0, 255);
+    // use the encoder position to set the state of an LED:
+    analogWrite(4, newPosition);
+    // print the value to the serial monitor:
+    Serial.println(newPosition);
+    // save the current position for comparison next time:
+    lastPosition = encoderPos;
+  }
+}
